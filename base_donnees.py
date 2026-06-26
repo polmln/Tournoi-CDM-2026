48ff04fd4c20b333b58950b82e062e8accfde687
polmln <pol.miolan42@gmail.com>
2026-06-26 09:36:43 +0200

    je sais pas

--- base_donnees.py
+++ base_donnees.py
@@ -17,6 +17,7 @@ def initialiser():
         CREATE TABLE IF NOT EXISTS equipe_preferee (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             equipe TEXT NOT NULL
+            date TEXT NOT NULL
         )
     """)
     connexion.commit()
